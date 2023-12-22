4afa1436f50b42422500945cadd30afe22d06c04
AryanSMD <aryansmd79@gmail.com>
2023-12-22 16:34:37 +0330

    initial design created

---
+++ src/components/CorrectingExam.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="wrapper">
+        <div class="head filial">
+            <h1>ПРОВЕРКА</h1>
+        </div>
+        <div class="content">
+            <div class="left-side">
+                <div class="top">
+                    <div class="title">Информация</div>
+                    <div class="box">
+                        <div class="item">
+                            <div class="name">ФИО</div>
+                            <!-- <div class="text">{{ student.user.fullName }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Пол</div>
+                            <!-- <div class="text">{{ sexType?.value }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Дата рождения</div>
+                            <!-- <div class="text">{{ student.user.birthDate }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Национальность</div>
+                            <!-- <div class="text">{{ student.user.nationality }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Город</div>
+                            <!-- <div class="text">{{ student.user.placeOfBirth }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Документ</div>
+                            <!-- <div class="text">{{ documentType?.value }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Дата выдачи</div>
+                            <!-- <div class="text">{{ student.user.issueDate }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Кем выдан</div>
+                            <!-- <div class="text">{{ student.user.issuedBy }}</div> -->
+                        </div>
+                        <div class="item">
+                            <div class="name">Миграционная карта</div>
+                            <!-- <div class="text">{{ student.user.migrationCard }}</div> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="img">
+                    <img src="" alt="">
+                </div>
+                <div class="bottom">
+                    <div class="headers">Оценка:</div>
+                    <input type="text" class="input" placeholder="Введите оценку">
+                    <div class="headers">Ваши комментарии</div>
+                    <textarea class="input comment"></textarea>
+                </div>
+            </div>
+            <div class="right-side">
+            </div>
+        </div>
+        <div class="footer">
+            <router-link to="/Users" type="button" class="btn cancel">Отменить</router-link>
+            <button type="button" class="btn">Сохранить</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+.content {
+    width: 100%;
+    display: flex;
+}
+.content .left-side{
+    margin-right: 20px;
+    display: flex;
+    width: 25%;
+    flex-direction: column;
+}
+.content .right-side{
+    width: 70%;
+    background-color: white;
+
+}
+.content .left-side .top {
+    background-color: white;
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+.content .left-side .top .box .item {
+    display: flex;
+    overflow: hidden;
+    margin-bottom: 10px;
+    line-height: 16px;
+}
+.content .left-side .top .box .item .name {
+    width: 50%;
+    color: #909090;
+}
+.content .left-side .top .box .item .text {
+    font-size: 14px;
+    width: 50%;
+}
+.content .left-side .img {
+    width: 100%;
+    height: 250px;
+    background-color: #C4C4C4;
+    margin-bottom: 30px;
+}
+.content .left-side .img img {
+    width: 100%;
+    height: auto;
+}
+.content .left-side .bottom {
+    display: flex;
+    flex-direction: column;
+}
+.content .left-side .bottom .headers {
+    font-size: 16px;
+    margin: 15px 0;
+}
+.content .left-side .bottom .comment {
+    width: 93%;
+}
+.footer {
+    margin: 50px 0;
+    display: flex;
+    justify-content: center;
+}
+.footer .btn {
+    display: flex;
+    cursor: pointer;
+    border: none;
+    justify-content: center;
+    margin-right: 50px;
+    width: 13%;
+}
+.footer .cancel {
+    background-color: #E6F0F9;
+    color: black;
+    width: 10%;
+}
+.footer .cancel:hover {
+    background-color: #2057A1;
+    color: white;
+}
+</style>
---
+++ src/components/UserExams.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="wrapper">
+        <div class="head filial">
+            <h1>ПРОВЕРКА</h1>
+        </div>
+        <div class="serch">
+            <form>
+                <div class="box">
+                    <input type="text" v-model="filter.period" placeholder="Выбрать период" onfocus="(this.type='date')" class="dats">
+                    <input type="text" v-model="filter.city" placeholder="Выбрать город" class="inputFilter">
+                    <select v-model="filter.resposibleId">
+                        <option value="" disabled selected>Выбрать уровень</option>
+                        <!-- <option value="" v-if=".length > 0">{{ allForDropdowns }}</option>
+                        <option v-for="item in " :key="item.key" :value="item.key">{{ item. }}</option> -->
+                    </select>
+                    <select v-model="filter.resposibleId">
+                        <option value="" disabled selected>Гражданство</option>
+                        <!-- <option value="" v-if=".length > 0">{{ allForDropdowns }}</option>
+                        <option v-for="item in " :key="item.key" :value="item.key">{{ item. }}</option> -->
+                    </select>
+                    <select v-model="filter.resposibleId">
+                        <option value="" disabled selected>Выбрать статус</option>
+                        <!-- <option value="" v-if=".length > 0">{{ allForDropdowns }}</option>
+                        <option v-for="item in " :key="item.key" :value="item.key">{{ item. }}</option> -->
+                    </select>
+                    <div class="bot">
+                        <button type="button" class="rezet" @click="resetFilters()">Сбросить фильтры</button>
+                    </div>
+                </div>
+                <div class="box">
+                    <input type="text" class="serch_in" v-model="filter.name" placeholder="Напишите запрос для поиска или код филиала">
+                    <span class="lup"></span>
+                    <button type="button" class="btn" @click="searchBranches()">Поиск</button>
+                </div>
+            </form>
+        </div>
+        <div class="vopros">
+            <div class="table filials">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>ФИО</th>
+                            <th>Уровень</th>
+                            <th>Национальность</th>
+                            <th>Город</th>
+                            <th>Статус</th>
+                            <th>Контакты</th>
+                        </tr>
+                        <!-- <tr v-for="item in " :key="item.id">
+                            <td>{{item.}}</td>
+                            <td>
+                                <router-link :to="``" @click="(item)" >{{item.name}}</router-link>
+                            </td>
+                            <td>{{item.}}</td>
+                            <td>{{item.}}</td>
+                            <td>{{item.}}</td>
+                            <td>{{item.}}</td>
+                            <td>{{item.}}</td>
+                        </tr> -->
+                    </tbody>
+                </table>
+            </div>
+            <!-- <ActPagination :totalPages="paging.totalPages" :perPage="this.defaultPaging.pageSize" :currentPage="currentPage"
+                :maxVisibleButtons="this.defaultPaging.maxVisibleButtons" @pagechanged="onPageChange" /> -->
+        </div>
+    </div>
+</template>
+
+
+<script>
+// import ActPagination from './elementComponents/ActPagination.vue'
+export default {
+    name: "UserExams",
+    data() {
+        return {
+            filter: {
+                name: '',
+                period: '',
+                city: '',
+                resposibleId: '',
+                page: 1,
+            },
+            currentPage: 1,
+        }
+    },
+    components: {
+        // ActPagination,
+    }
+}
+</script>
+
+
+<style>
+.filial.head+.serch .box select+select {
+    max-width: 196px;
+}
+</style>
